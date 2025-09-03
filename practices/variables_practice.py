69cc36edab44ebce22d26834eb2d9435a9929b1d
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-03 13:44:39 -0600

    tested code space

--- practices/variables_practice.py
+++ practices/variables_practice.py
@@ -1,13 +1,18 @@
 # AT 6th Variables Practice
 name=input("what is your name")
+
 name=input("a number between 1 and ten")
+
 name=input("what did you have for breakfeast")
+
 name=input("whats your favorite color")
+
 name=input("the school name")
+
 name=input("the year")
+
 name=input("your eye color")
+
 name=input("your age")
+
 name=input("your favortie subject in school")
-
-
-
