3fefdd09f7b97cd3766dde621929d4717668c2cc
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-04 13:43:49 -0600

    started expression notes

---
+++ notes/expressions.py
@@ -0,0 +1,38 @@
+# AT 6th Expression Notes!
+
+# what is an algorthim?
+    # an algorithm is a set of instructions to accomplish a task 
+# list steps in an algorithm 
+    # 1.Variables/pieaces of info needed 2. process info 3. provide output
+
+#list all of the different mathematical operators
+    # +, -, /, *
+
+    
+name = input("What is your name?")
+print("hello", name)
+
+length = 5
+width = 20
+area = length * width 
+print(length, "*", width, "=", area)
+#1
+age1 = 14
+age2 = 15
+age3= 16
+age4 = 17
+#2
+averge = (age1 + age2 + age3 + age4)/4
+#3
+print(averge)
+print("the averge age between;", age1, age2, age3, age4, "is 15.5")
+
+#math equations!
+num_one = 12
+num_two = 4
+
+print("addition:", num_one + num_two)
+print("subtraction:", num_one - num_two)
+print("exponets:", num_one ** num_two)
+print("integer division:", num_one // num_two)
+print("modulo (remainder): ", num_one % num_two)
