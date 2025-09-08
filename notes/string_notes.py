f5b488aa30e7cc76e25cf0c8026afc64e0a70fee
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-08 13:44:21 -0600

    added code

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -1,2 +1,21 @@
 # AT 6th string notes
+
 print("i did it!")
+
+# 1 what makes something a string
+#   a collection of symbols that are held togehter by quotation marks
+# 2 why do we have strings
+#   
+# 3 how do we stupid proofing and santization relate to eachother?
+
+# 4 what is debugging
+
+# 5 how do you debug the different types of errors
+
+# 6 describe what each method below does
+    # strip get rids of any extra spaces the user puts at the end or start of string
+name = input("what is your name?:/n").strip().lower().upper()
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+print("Welcome to my program", name, )
