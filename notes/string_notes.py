99d739f68b3def55137c37677946a56d617b504f
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-09 13:44:24 -0600

    added code

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -10,9 +10,17 @@ print("i did it!")
 # 3 how do we stupid proofing and santization relate to eachother?
 
 # 4 what is debugging
-#   Debugging is ficing problems in my code
+#   Debugging is fixing problems in my code
+    # syntax error
+    #anytime where we have written our code wrong and our computer cannot read it
+    # logic error
+    #when our code does something that we do not expect
+    # Run-time error
+    # when there is a problem in the code that will break when it tries to run
 # 5 how do you debug the different types of errors
-
+    #
+# a bug is any error in the code that keeps the code from running properly
+    #you need to double check over code and look for, syntax, logic, or a runtime error
 # 6 describe what each method below does
     # strip get rids of any extra spaces the user puts at the end or start of string
 name = input("what is your name?:/n").strip().lower().upper()
