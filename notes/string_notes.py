4fe4010a1a9a0bb7ec7391acba5a507cca7d6adb
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-09 13:03:30 -0600

    added debugging definition

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -1,6 +1,7 @@
 # AT 6th string notes
 
 print("i did it!")
+# it is a name generator, the sanatize lines are confusing me, the inputs
 
 # 1 what makes something a string
 #   a collection of symbols that are held togehter by quotation marks
@@ -9,7 +10,7 @@ print("i did it!")
 # 3 how do we stupid proofing and santization relate to eachother?
 
 # 4 what is debugging
-
+#   Debugging is ficing problems in my code
 # 5 how do you debug the different types of errors
 
 # 6 describe what each method below does
@@ -19,3 +20,4 @@ name = input("what is your name?:/n").strip().lower().upper()
 sentence = "The quick brown fox jumps over the lazy dog"
 
 print("Welcome to my program", name, )
+
