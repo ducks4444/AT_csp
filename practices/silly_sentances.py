cd49272a9ed75d445cb3d38526f5b660525855e0
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-10 13:37:51 -0600

    finished code

---
+++ practices/silly_sentances.py
@@ -0,0 +1,8 @@
+name=input("what is your name  ")
+animal1=input("name a type of animal")
+limb=input("name a type of body limb")
+sauce=input("name a type of sauce")
+
+print(name, "was walking their", animal1, "when they had tripped and snapped their",limb, "and they bled out",sauce+".")
+print(animal1, "was running away when they found", sauce, "and when they consumed the cause their", limb, "had popped off and then a person named", name, "had put it back on.")
+print(name, "was walking around till with",sauce,"in their hand till a", animal1, "came and bit their finger to try and get the", sauce+".")
