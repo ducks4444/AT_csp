2ca4adb63468678f5d690e70d98b49ffa331035a
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-17 13:43:47 -0600

    added code

---
+++ notes/conditional_notes.py
@@ -0,0 +1,30 @@
+# AT 6th condtion notes
+
+# 1 what puts something insdie of a if statment
+    # a true or false statment
+# 2 what do if statments do
+    # they check true or flase statment
+# 3 what do boolean statments do
+    # something that is always either true or false
+# 4 what do else statments do
+    # basiclly take the if statement if its not true or false it will make it something else
+# 5 what kind of statement do you use if you have more than 2 needed statements
+    # when our code is tabbed over its in our if statement if not its not or you wanna check more than one thing
+# 6 what do these symbols mean
+    # < less than, greater than >, >= greater than or equal to, < less than or equal to
+# use to equal signs to see if its equal so it doesnt think its a variable, you can check if its not equal to by a ! and = 
+#7 what are the 3 logical operators
+    # 
+# elif means else if 
+num = 6
+
+if num < 10:
+    print(f"{num} is a single digit number")
+else:
+    print(f"{num} is not a single digit number")
+name = "mike"
+if name == "mike":
+    print("you are a student")
+else:
+    print(f"hello {name} you are a student")
+
