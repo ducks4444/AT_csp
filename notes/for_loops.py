e49a99db9aef06a4ddaeeba04cb29a1bbc88c534
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-19 13:43:30 -0600

    Added code

---
+++ notes/for_loops.py
@@ -0,0 +1,13 @@
+# AT 6th loop notes 
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+print(f"the time is: {current}")
+print(f"The hour is: {hour}")
+
+# lists
+    # allows you to store multiple pieaces of information within the same thing
+siblings = ["micheal", "will", "kayson", "murphy", "ander", "covery", "reia"]
---
+++ practices/time.py
@@ -0,0 +1,9 @@
+# AT what time is it practice
+
+time=int(input("what time is it?:\n"))
+
+if time < 11:
+    print(f" its {time} good morning")
+elif time > 11:
+    print(f" its {time} goodafternooon")
+elif time 
