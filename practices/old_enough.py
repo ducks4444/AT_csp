beecbb54642fc3a9ca950c86c6735bc269978c76
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-22 10:19:16 -0600

    finished code

--- practices/old_enough.py
+++ practices/old_enough.py
@@ -1,4 +1,14 @@
 # AT 6th old enough python
 
-input = print("what is your age")
-age = 9
+age=int(input("what is your age?:\n"))
+
+if age > 18:
+    print(f" you are {age}, your old enough to vote, go to school, get a learners permit, drive, and vote")
+elif age > 16:
+    print(f" you are {age}, you are old enough to get a learners permit, drive, and go to school.")
+elif age > 15:
+    print(f" you are {age}, you are old enough to get a learners permit and go to school.")
+elif age > 5:
+    print(f" you are {age}, you are old enough to go to school")
+
+
