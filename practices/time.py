8092a03eb52178071958bdb24bc6112a51e5f80a
ducks4444 <adelle.terry@ucas-edu.net>
2025-09-22 10:08:21 -0600

    finished code

--- practices/time.py
+++ practices/time.py
@@ -1,9 +1,10 @@
 # AT what time is it practice
 
-time=int(input("what time is it?:\n"))
+time=int(input("what time is it in militart time?:\n"))
 
 if time < 11:
     print(f" its {time} good morning")
 elif time > 11:
     print(f" its {time} goodafternooon")
-elif time 
+elif time > 4:
+    print(f" its {time} good evening")
