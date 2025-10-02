cb1f877941344e5c8e0e16f77f8d4037026d6e79
ducks4444 <adelle.terry@ucas-edu.net>
2025-10-02 13:43:59 -0600

    added notes

---
+++ c_programming/note/variables.c
@@ -0,0 +1,14 @@
+// AT 6th  variable notes
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A'; //will only hold one letter
+    char name[]= "adelle";
+    //bool passing = true;
+    //a semi colon serves as a period that ends a thought basiclly
+    printf("%d", num);
+    return 0;
+}
+// we have to tell the compuetr what type of data there is static and num 
