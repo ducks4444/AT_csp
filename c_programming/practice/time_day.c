bf567256273c2786e7a1a5b4c6997bbc2cd97528
ducks4444 <adelle.terry@ucas-edu.net>
2025-10-13 10:49:44 -0600

    added code

--- c_programming/practice/time_day.c
+++ c_programming/practice/time_day.c
@@ -1,1 +1,13 @@
 // AT 6th time of day assignment
+
+#inlcude <stdio.h> 
+int main void (){
+    printf("what hour is it in military time?: ");
+    scanf("s%" time );
+    if time >=12
+    printf("good afternoon :");
+    else if time >= 5
+    printf("good evening :");
+    else if
+
+}
