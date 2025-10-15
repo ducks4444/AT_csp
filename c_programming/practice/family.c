5df89b67730ef6b8a0dec7864e49444c48ea08e6
ducks4444 <adelle.terry@ucas-edu.net>
2025-10-14 22:15:45 -0600

    Update family.c

--- c_programming/practice/family.c
+++ c_programming/practice/family.c
@@ -3,7 +3,7 @@
 
 int main() {
     char *family[] = {"mike", "reia", "thor"};
-    for (int i = 0; i < ; i++)
+    for (int i = 0; i < 3; i++)
         printf("Hi %s!\n", family[i]);
     return 0;
 }
