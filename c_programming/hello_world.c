2607eb3c1b4c1ff672426f8fee9a73c6d9bb6a4f
ducks4444 <adelle.terry@ucas-edu.net>
2025-10-15 13:28:45 -0600

    Add hello_world.c to greet multiple names

---
+++ c_programming/hello_world.c
@@ -0,0 +1,19 @@
+// AT 6th hello world update
+
+#include <stdio.h>
+void greet(const char name[]){
+    printf("hello %s\n\n", name);
+
+
+}
+
+int main() {
+    greet("mike");
+    greet("mayzi");
+    greet("kenji");
+    greet("clarise");
+    greet("adelide");
+
+    return 0;
+
+}
