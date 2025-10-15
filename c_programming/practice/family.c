88a29f931860654ec21a2a6d836ce8e7727b2373
ducks4444 <adelle.terry@ucas-edu.net>
2025-10-14 21:57:38 -0600

    Update family.c

--- c_programming/practice/family.c
+++ c_programming/practice/family.c
@@ -1,1 +1,9 @@
 // AT 6th family assignment
+#include <stdio.h>
+
+int main() {
+    char *family[] = {"mike", "reia", "thor"};
+    for (int i = 0; i < ; i++)
+        printf("Hi %s!\n", family[i]);
+    return 0;
+}
